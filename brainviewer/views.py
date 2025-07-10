26202d93afff63ab4ab3ab940002faaf6df2c320
Imran <SAM8402@users.noreply.github.com>
2025-07-10 10:39:36 +0530

    some update and error handel

--- brainviewer/views.py
+++ brainviewer/views.py
@@ -118,7 +118,7 @@ def get_transformation_data(biosample_id, section_number_str):
 
 def get_available_biosample_ids():
     # return ["222"]
-    return ["222","244"]
+    return ["222", "244"]
 
 
 def get_available_slice_numbers_for_biosample(biosample_id):
@@ -358,6 +358,8 @@ def viewer_view_test(request, biosample_id, slice_number_str, port_no=10803):
         f"Data being passed to template for /viewer/{biosample_id}/{slice_number_str}: {template_data}")
 
     return render(request, 'brainviewer/test_viewer_v1.html', template_data)
+
+
 def viewer_view_split(request, biosample_id, slice_number_str, port_no=10803):
     logger.info(
         f"Received request for /viewer/{biosample_id}/{slice_number_str}")
@@ -369,85 +371,172 @@ def viewer_view_split(request, biosample_id, slice_number_str, port_no=10803):
             f"'slice_number' in URL must be an integer, got: {slice_number_str}")
         raise Http404(
             f"'slice_number' ({slice_number_str}) must be an integer.")
+    
+    if biosample_id not in get_available_biosample_ids():
+        logger.warning(f"Invalid biosample_id: {biosample_id}")
+        html_content = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>No Brain ID Found</title>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                    padding: 40px;
+                }}
+                .error-box {{
+                    background-color: #fff;
+                    border: 1px solid #ccc;
+                    padding: 30px;
+                    display: inline-block;
+                    border-radius: 10px;
+                    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+                }}
+                a {{
+                    display: block;
+                    margin-top: 20px;
+                    text-decoration: none;
+                    color: #007BFF;
+                    font-weight: bold;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="error-box">
+                <h2>🚫 Biosample ID Not Found</h2>
+                <p>The ID <strong>{biosample_id}</strong> does not exist in our records.</p>
+            </div>
+        </body>
+        </html>
+        """
+        return HttpResponse(html_content)
+    elif slice_number_str not in get_available_slice_numbers_for_biosample(biosample_id):
+        logger.warning(f"Invalid slice_id: {slice_number_str}")
+        html_content = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>No Slice ID Found</title>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                    padding: 40px;
+                }}
+                .error-box {{
+                    background-color: #fff;
+                    border: 1px solid #ccc;
+                    padding: 30px;
+                    display: inline-block;
+                    border-radius: 10px;
+                    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+                }}
+                a {{
+                    display: block;
+                    margin-top: 20px;
+                    text-decoration: none;
+                    color: #007BFF;
+                    font-weight: bold;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="error-box">
+                <h2>🚫 Slice ID Not Found</h2>
+                <p>The ID <strong>{slice_number_str}</strong> does not exist in our records.</p>
+            </div>
+        </body>
+        </html>
+        """
+        return HttpResponse(html_content)
+    else:
+        jp2_meta = get_jp2_metadata(biosample_id, slice_number_int)
+        transform_data = get_transformation_data(biosample_id, slice_number_str)
+        if not transform_data:
+            raise Http404(
+                f"Transformation Data not found for biosample {biosample_id}, slice {slice_number_str}.")
 
-    jp2_meta = get_jp2_metadata(biosample_id, slice_number_int)
-    transform_data = get_transformation_data(biosample_id, slice_number_str)
-    if not transform_data:
-        raise Http404(
-            f"Transformation Data not found for biosample {biosample_id}, slice {slice_number_str}.")
+        if not jp2_meta:
+            logger.error(
+                f"Failed to get JP2 metadata for biosample {biosample_id}, slice {slice_number_str}.")
+            raise Http404(
+                f"JP2 Data not found for biosample {biosample_id}, slice {slice_number_str}.")
+        if not transform_data:
+            logger.error(
+                f"Failed to get transformation data for biosample {biosample_id}, slice {slice_number_str}.")
+            raise Http404(
+                f"Transformation Data not found for biosample {biosample_id}, slice {slice_number_str}.")
 
-    if not jp2_meta:
-        logger.error(
-            f"Failed to get JP2 metadata for biosample {biosample_id}, slice {slice_number_str}.")
-        raise Http404(
-            f"JP2 Data not found for biosample {biosample_id}, slice {slice_number_str}.")
-    if not transform_data:
-        logger.error(
-            f"Failed to get transformation data for biosample {biosample_id}, slice {slice_number_str}.")
-        raise Http404(
-            f"Transformation Data not found for biosample {biosample_id}, slice {slice_number_str}.")
+        h_jp2_to_bfi = transform_data.get("H_jp2_to_bfi")
+        bfi_natural_dims = transform_data.get("bfi_natural_dims")
 
-    h_jp2_to_bfi = transform_data.get("H_jp2_to_bfi")
-    bfi_natural_dims = transform_data.get("bfi_natural_dims")
+        if not h_jp2_to_bfi:
+            logger.error(
+                f"H_jp2_to_bfi matrix is missing from transformation data for biosample {biosample_id}, slice {slice_number_str}.")
+            raise Http404(
+                f"Critical transformation matrix H_jp2_to_bfi is missing for slice {slice_number_str}.")
+        if not bfi_natural_dims or not isinstance(bfi_natural_dims, list) or len(bfi_natural_dims) < 2:
+            logger.error(
+                f"BFI natural dimensions are missing or invalid from transformation data for biosample {biosample_id}, slice {slice_number_str}.")
+            raise Http404(
+                f"Critical BFI natural dimensions are missing or invalid for slice {slice_number_str}.")
 
-    if not h_jp2_to_bfi:
-        logger.error(
-            f"H_jp2_to_bfi matrix is missing from transformation data for biosample {biosample_id}, slice {slice_number_str}.")
-        raise Http404(
-            f"Critical transformation matrix H_jp2_to_bfi is missing for slice {slice_number_str}.")
-    if not bfi_natural_dims or not isinstance(bfi_natural_dims, list) or len(bfi_natural_dims) < 2:
-        logger.error(
-            f"BFI natural dimensions are missing or invalid from transformation data for biosample {biosample_id}, slice {slice_number_str}.")
-        raise Http404(
-            f"Critical BFI natural dimensions are missing or invalid for slice {slice_number_str}.")
+        jp2_base_url = "https://apollo2.humanbrain.in/iipsrv/fcgi-bin/iipsrv.fcgi?FIF=/"
+        jp2_path = jp2_meta['jp2_path_fragment']
+        if jp2_path.startswith('/'):
+            jp2_path = jp2_path[1:]
 
-    jp2_base_url = "https://apollo2.humanbrain.in/iipsrv/fcgi-bin/iipsrv.fcgi?FIF=/"
-    jp2_path = jp2_meta['jp2_path_fragment']
-    if jp2_path.startswith('/'):
-        jp2_path = jp2_path[1:]
+        jp2_suffix = "&WID=1024&GAM=1.4&MINMAX=1:0,255&MINMAX=2:0,255&MINMAX=3:0,255&JTL={z},{tileIndex}"
+        full_jp2_url = f"{jp2_base_url}{jp2_path}{jp2_suffix}"
 
-    jp2_suffix = "&WID=1024&GAM=1.4&MINMAX=1:0,255&MINMAX=2:0,255&MINMAX=3:0,255&JTL={z},{tileIndex}"
-    full_jp2_url = f"{jp2_base_url}{jp2_path}{jp2_suffix}"
+        # bfi_image_url = f"/static/images_data/{biosample_id}/bfi-{slice_number_str}.png"
+        # bfi_image_url = f"images_data/{biosample_id}/bfi-{slice_number_str}.png"   # this is not working
+        # bfi_image_url = f"http://dgx3.humanbrain.in:10803/images/222/bfi-763.png"
+        bfi_image_url = f"http://dgx3.humanbrain.in:{port_no}/images/{biosample_id}/bfi-{slice_number_str}.png"
+        logger.info(f"Generated BFI Image URL: {bfi_image_url}")
 
-    # bfi_image_url = f"/static/images_data/{biosample_id}/bfi-{slice_number_str}.png"
-    # bfi_image_url = f"images_data/{biosample_id}/bfi-{slice_number_str}.png"   # this is not working
-    # bfi_image_url = f"http://dgx3.humanbrain.in:10803/images/222/bfi-763.png"
-    bfi_image_url = f"http://dgx3.humanbrain.in:{port_no}/images/{biosample_id}/bfi-{slice_number_str}.png"
-    logger.info(f"Generated BFI Image URL: {bfi_image_url}")
+        available_b_ids = get_available_biosample_ids()
+        available_s_nums = get_available_slice_numbers_for_biosample(biosample_id)
 
-    available_b_ids = get_available_biosample_ids()
-    available_s_nums = get_available_slice_numbers_for_biosample(biosample_id)
+        if slice_number_str not in available_s_nums and available_s_nums:
+            logger.warning(
+                f"Current slice {slice_number_str} for biosample {biosample_id} is not in the dynamically generated available_slice_numbers list: {available_s_nums}. The dropdown might not pre-select it correctly if it's missing from the JSON keys.")
 
-    if slice_number_str not in available_s_nums and available_s_nums:
-        logger.warning(
-            f"Current slice {slice_number_str} for biosample {biosample_id} is not in the dynamically generated available_slice_numbers list: {available_s_nums}. The dropdown might not pre-select it correctly if it's missing from the JSON keys.")
+        bfi_w = bfi_natural_dims[0] if bfi_natural_dims and len(
+            bfi_natural_dims) > 0 else 0
+        bfi_h = bfi_natural_dims[1] if bfi_natural_dims and len(
+            bfi_natural_dims) > 1 else 0
+        print("BFI image url:", json.dumps(bfi_image_url))
+        
+        mri_image_url = full_jp2_url
+        nissl_image_url = bfi_image_url
+        # nissl_image_url = full_jp2_url
+        
+        template_data = {
+            "jp2_map_url": json.dumps(full_jp2_url),
+            "jp2_full_size": json.dumps([jp2_meta["width"], jp2_meta["height"]]),
+            "jp2_initial_view_rotation_deg": json.dumps(jp2_meta["rotation"]),
+            "bfi_image_url": json.dumps(bfi_image_url),
+            "bfi_css_rotation_deg": json.dumps(jp2_meta["rotation"]),
+            "h_jp2_to_bfi": json.dumps(h_jp2_to_bfi),
+            "bfi_natural_width": json.dumps(bfi_w),
+            "bfi_natural_height": json.dumps(bfi_h),
+            "test_points_jp2": [],
+            "title": f"Slice Viewer - BSID {biosample_id}, Slice {slice_number_str}",
+            "current_biosample_id": json.dumps(biosample_id),
+            "current_slice_number": json.dumps(slice_number_str),
+            "available_biosample_ids": json.dumps(available_b_ids),
+            "available_slice_numbers": json.dumps(available_s_nums),
+            "mri_image_url": json.dumps(mri_image_url),
+            "nissl_image_url": json.dumps(nissl_image_url),
+            "viewer_url_template": json.dumps(f"/viewer/__BID__/__SID__/")
+        }
+        logger.debug(
+            f"Data being passed to template for /viewer/{biosample_id}/{slice_number_str}: {template_data}")
 
-    bfi_w = bfi_natural_dims[0] if bfi_natural_dims and len(
-        bfi_natural_dims) > 0 else 0
-    bfi_h = bfi_natural_dims[1] if bfi_natural_dims and len(
-        bfi_natural_dims) > 1 else 0
-    print("BFI image url:", json.dumps(bfi_image_url))
-    template_data = {
-        "jp2_map_url": json.dumps(full_jp2_url),
-        "jp2_full_size": json.dumps([jp2_meta["width"], jp2_meta["height"]]),
-        "jp2_initial_view_rotation_deg": json.dumps(jp2_meta["rotation"]),
-        "bfi_image_url": json.dumps(bfi_image_url),
-        "bfi_css_rotation_deg": json.dumps(jp2_meta["rotation"]),
-        "h_jp2_to_bfi": json.dumps(h_jp2_to_bfi),
-        "bfi_natural_width": json.dumps(bfi_w),
-        "bfi_natural_height": json.dumps(bfi_h),
-        "test_points_jp2": [],
-        "title": f"Slice Viewer - BSID {biosample_id}, Slice {slice_number_str}",
-        "current_biosample_id": json.dumps(biosample_id),
-        "current_slice_number": json.dumps(slice_number_str),
-        "available_biosample_ids": json.dumps(available_b_ids),
-        "available_slice_numbers": json.dumps(available_s_nums),
-        # CORRECTED PARAMETER NAME HERE
-        "viewer_url_template": json.dumps(f"/viewer/__BID__/__SID__/")
-    }
-    logger.debug(
-        f"Data being passed to template for /viewer/{biosample_id}/{slice_number_str}: {template_data}")
-
-    return render(request, 'brainviewer/viewer_working_v1.html', template_data)
-    # return render(request, 'brainviewer/no_complete_sync.html', template_data)
-    # return render(request, 'brainviewer/test.html', template_data)
+        return render(request, 'brainviewer/viewer_working_v1.html', template_data)
+        # return render(request, 'brainviewer/no_complete_sync.html', template_data)
+        # return render(request, 'brainviewer/test.html', template_data)
